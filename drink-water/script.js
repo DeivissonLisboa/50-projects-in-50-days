da2082e73f87a8ed67cf88fa5b7ad432f79cea6e
Deivisson Lisboa <dvilaflor@gmail.com>
2022-12-30 08:45:35 -0300

    create big cup update function

--- drink-water/script.js
+++ drink-water/script.js
@@ -8,6 +8,13 @@ smallCups.forEach((cup, i) => {
 })
 
 function fillCup(index) {
+  if (
+    smallCups[index].classList.contains("full") &&
+    !smallCups[index].nextElementSibling.classList.contains("full")
+  ) {
+    index--
+  }
+
   smallCups.forEach((cup, i) => {
     if (i <= index) {
       cup.classList.add("full")
@@ -15,4 +22,31 @@ function fillCup(index) {
       cup.classList.remove("full")
     }
   })
+
+  updateCup()
 }
+
+function updateCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length
+  const cups = smallCups.length
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden"
+    percentage.style.height = 0
+  } else {
+    percentage.style.visibility = "visible"
+    const cupsPercentage = `${(fullCups * 100) / cups}%`
+    percentage.style.height = cupsPercentage
+    percentage.innerText = cupsPercentage
+  }
+
+  if (fullCups === cups) {
+    remained.style.visibility = "hidden"
+    remained.style.height = 0
+  } else {
+    remained.style.visibility = "visible"
+    liters.innerText = `${2 - fullCups * 0.25}L`
+  }
+}
+
+updateCup()
