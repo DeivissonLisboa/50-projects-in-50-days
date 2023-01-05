f678e09c759d2a8a39178849bb69b53ca2a14881
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-05 14:58:50 -0300

    add buttons eventListener

---
+++ button-ripple-effect/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll(".ripple")
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const x = e.clientX
+    const y = e.clientY
+
+    const buttonLeft = e.target.offsetLeft
+    const buttonTop = e.target.offsetTop
+
+    const xInside = x - buttonLeft
+    const yInside = y - buttonTop
+
+    const circle = document.createElement("span")
+    circle.classList.add("circle")
+    circle.style.left = `${xInside}px`
+    circle.style.top = `${yInside}px`
+
+    e.target.appendChild(circle)
+
+    setTimeout(() => circle.remove(), 500)
+  })
+})
