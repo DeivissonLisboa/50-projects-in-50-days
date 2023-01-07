2808479bd091010d118f534ae1d4a85664697110
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-07 19:57:24 -0300

    add controls eventListeners

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -1,9 +1,14 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+const decrease = document.getElementById("decrease")
+const sizeDisplay = document.getElementById("size")
+const increase = document.getElementById("increase")
+const colorPicker = document.getElementById("color")
+const clear = document.getElementById("clear")
 
 let size = 20
 let isPressed = false
-let color = "#000"
+let color = colorPicker.value
 let x, y
 
 function drawCircle(x, y) {
@@ -45,4 +50,28 @@ canvas.addEventListener("mousemove", (e) => {
     x = x_2
     y = y_2
   }
+})
+
+decrease.addEventListener("click", () => {
+  if (size > 5) {
+    size -= 5
+  }
+
+  sizeDisplay.innerText = size
+})
+
+increase.addEventListener("click", () => {
+  if (size < 20) {
+    size += 5
+  }
+
+  sizeDisplay.innerText = size
+})
+
+colorPicker.addEventListener("change", () => {
+  color = colorPicker.value
+})
+
+clear.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 })
