c7b5f70f7188b4a1920ebe299a428822829c83c1
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-07 19:45:07 -0300

    create canvas eventListeners

--- drawing-app/script.js
+++ drawing-app/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 let size = 20
+let isPressed = false
 let color = "#000"
 let x, y
 
@@ -17,6 +18,31 @@ function drawLine(x_1, y_1, x_2, y_2) {
   ctx.moveTo(x_1, y_1)
   ctx.lineTo(x_2, y_2)
   ctx.strokeStyle = color
-  ctx.lineWidth = size
+  ctx.lineWidth = size * 2
   ctx.stroke()
 }
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true
+  x = e.offsetX
+  y = e.offsetY
+})
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false
+  x = undefined
+  y = undefined
+})
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x_2 = e.offsetX
+    const y_2 = e.offsetY
+
+    drawCircle(x_2, y_2)
+    drawLine(x, y, x_2, y_2)
+
+    x = x_2
+    y = y_2
+  }
+})
