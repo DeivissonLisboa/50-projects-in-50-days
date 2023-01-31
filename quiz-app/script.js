13fbde25d81a6f8c4921245d28337dd6cca08093
Deivisson Lisboa <dvilaflor@gmail.com>
2023-01-31 18:15:49 -0300

    Create quizLoop function

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,12 +1,17 @@
 const QUIZ_HEADER = document.querySelector(".quiz-header")
+const SUBMIT_BUTTON = document.getElementById("submit")
 const API_URL = "https://the-trivia-api.com/api/questions"
+let questions
+let currentQuention = 0
 let selectAnswer
 
-function createQuestion(questionTitle, questionAnswers) {
-  QUIZ_HEADER.innerHTML = `<h2 id="question">${questionTitle}</h2>`
+function createQuestion(question, category, answers) {
+  QUIZ_HEADER.innerHTML = `
+    <h2 id="question">${question} <small>${category}</small></h2>    
+  `
 
   let questionsUl = document.createElement("ul")
-  questionAnswers.forEach((answer, index) => {
+  answers.forEach((answer, index) => {
     questionsUl.innerHTML += `
       <li>
         <input class="answer" type="radio" name="answer" id="answer${index}" />
@@ -19,3 +24,34 @@ function createQuestion(questionTitle, questionAnswers) {
 
   QUIZ_HEADER.append(questionsUl)
 }
+
+function scrambleAnswers(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+
+  return array
+}
+
+function nextQuestion() {
+  let { question, category, correctAnswer, incorrectAnswers } =
+    questions[currentQuention]
+
+  incorrectAnswers.push(correctAnswer)
+
+  let answers = scrambleAnswers(incorrectAnswers)
+
+  createQuestion(question, category, answers)
+}
+
+async function getQuestions() {
+  let response = await fetch(API_URL)
+  let data = await response.json()
+  questions = data
+  nextQuestion()
+}
+
+getQuestions()
