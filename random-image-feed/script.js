73a40b246bfa13ca527310fedcedbde957b54b8d
Deivisson Lisboa <dvilaflor@gmail.com>
2023-02-02 23:27:22 -0300

    Get square images for better grid

--- random-image-feed/script.js
+++ random-image-feed/script.js
@@ -2,16 +2,19 @@ const IMAGES_SECTION = document.querySelector("section")
 const UNSPLASH_URL = "https://source.unsplash.com/random/"
 
 function getRandomThreeDigits() {
-  return Math.floor(Math.random() * 100)
+  return Math.floor(Math.random() * 100) + 300
 }
 
 function getImage() {
   let image = document.createElement("img")
   image.classList.add("shadow-400")
-  image.src = UNSPLASH_URL + getRandomThreeDigits()
+
+  let imgSize = getRandomThreeDigits()
+
+  image.src = UNSPLASH_URL + `${imgSize}x${imgSize}`
   IMAGES_SECTION.append(image)
 }
 
-for (let i = 0; i < 10; i++) {
-  getImage()
+for (let i = 0; i < 30; i++) {
+  setTimeout(getImage, 50)
 }
